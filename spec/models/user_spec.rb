315fb93af1c949a0ce7a42329ea8769df192ffed
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-14 19:49:14 +0900

    テストコードの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -80,6 +80,59 @@ RSpec.describe User, type: :model do
         another_user.valid?
         expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
+
+      it 'メールアドレスに@を含まない場合は登録できない' do
+        @user.email = 'sss'
+        @user.valid?
+      end
+
+      it 'パスワードが6文字未満では登録できない' do
+        @user.password ='ooo'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+
+      it '英字のみのパスワードでは登録できない' do
+        @user.password ='aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+      it '数字のみのパスワードでは登録できない' do
+        @user.password ='111111'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+      it '全角文字を含むパスワードでは登録できない' do
+        @user.password ='1q2w3E'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+      it '姓（全角）に半角文字が含まれていると登録できない' do
+        @user.last_name = 'ﾀﾅｶ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name is invalid")
+      end
+
+      it '名（全角）に半角文字が含まれていると登録できない' do
+        @user.first_name ='ｶｸｴｲ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name is invalid")
+      end
+
+      it '姓（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
+        @user.reading_last_name = 'aあ阿1!'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Reading last name is invalid")
+      end
+
+      it '名（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
+        @user.reading_first_name = 'aあ阿1!'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Reading first name is invalid")
+      end
     end
   end
 end
