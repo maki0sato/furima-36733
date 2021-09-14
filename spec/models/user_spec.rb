5914e7208a8e4fbc61bcebe887fb7fdff160d0bb
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-14 19:53:30 +0900

    Rubocopを使用

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -87,25 +87,25 @@ RSpec.describe User, type: :model do
       end
 
       it 'パスワードが6文字未満では登録できない' do
-        @user.password ='ooo'
+        @user.password = 'ooo'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
 
       it '英字のみのパスワードでは登録できない' do
-        @user.password ='aaaaaa'
+        @user.password = 'aaaaaa'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
 
       it '数字のみのパスワードでは登録できない' do
-        @user.password ='111111'
+        @user.password = '111111'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
 
       it '全角文字を含むパスワードでは登録できない' do
-        @user.password ='1q2w3E'
+        @user.password = '1q2w3E'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
@@ -113,25 +113,25 @@ RSpec.describe User, type: :model do
       it '姓（全角）に半角文字が含まれていると登録できない' do
         @user.last_name = 'ﾀﾅｶ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name is invalid")
+        expect(@user.errors.full_messages).to include('Last name is invalid')
       end
 
       it '名（全角）に半角文字が含まれていると登録できない' do
-        @user.first_name ='ｶｸｴｲ'
+        @user.first_name = 'ｶｸｴｲ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name is invalid")
+        expect(@user.errors.full_messages).to include('First name is invalid')
       end
 
       it '姓（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
         @user.reading_last_name = 'aあ阿1!'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Reading last name is invalid")
+        expect(@user.errors.full_messages).to include('Reading last name is invalid')
       end
 
       it '名（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
         @user.reading_first_name = 'aあ阿1!'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Reading first name is invalid")
+        expect(@user.errors.full_messages).to include('Reading first name is invalid')
       end
     end
   end
