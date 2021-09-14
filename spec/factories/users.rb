e6b207b9ebe8663d8952de1051b8071b9e38c7d8
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-14 15:58:18 +0900

    FactoryBot修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)} 
-    password_confirmation {password}
-    last_name             {Faker::Last_name.initials(number: 2)}
-    first_name            {Faker::First_name.initials(number: 2)}
-    reading_last_name     {Faker::Reading_last_name.initials(number: 2)}
-    reading_first_name    {Faker::Reading_first_name.initials(number: 2)}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) } 
+    password_confirmation { password }
+    last_name             { person.last.kanji }
+    first_name            { person.first.kanji }
+    reading_last_name     { person.first.katakana }
+    reading_first_name    { person.first.katakana }
     date                  {Faker::Date.backward}
   end
 end
