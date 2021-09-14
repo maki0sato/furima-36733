f896855c8b36a7b7bc4a4855234a39c204beb926
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-14 14:04:16 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname,           presence: true
-  validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :reading_last_name,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :reading_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday,           presence: true
+         validates :nickname,           presence: true
+         validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :reading_last_name,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+         validates :reading_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+         validates :birthday,           presence: true
   
 end
