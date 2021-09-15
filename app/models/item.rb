4b438dff5eb33104617b6c7dabdc2e9751f4bc40
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-15 14:34:22 +0900

    itemモデルに各ActiveHashとのアソシエーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ApplicationRecord
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :days_to
+  belongs_to :delivery_free
+  belongs_to :prefecture
+  belongs_to :quality
+
   belongs_to :user
   has_one_attached :image
   
