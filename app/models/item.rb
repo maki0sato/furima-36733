ed1fb1e03663e47dedeb4f1054e74a00f6ad1112
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-15 14:36:45 +0900

    itemモデルのバリデーションに追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,11 +12,11 @@ class Item < ApplicationRecord
   
   validates :item,              presence: true
   validates :text,              presence: true
-  validates :category_id,       presence: true
-  validates :quality_id,        presence: true
-  validates :delivery_free_id,  presence: true
-  validates :prefecture_id,     presence: true
-  validates :days_to_id,        presence: true
+  validates :category_id,       presence: true, numericality: { other_than: 1 }
+  validates :quality_id,        presence: true, numericality: { other_than: 1 }
+  validates :delivery_free_id,  presence: true, numericality: { other_than: 1 }
+  validates :prefecture_id,     presence: true, numericality: { other_than: 1 }
+  validates :days_to_id,        presence: true, numericality: { other_than: 1 }
   validates :price,             presence: true, numericality: {in: 300..9999999}
   validates :price,             format: { with: /\A[0-9]+\z/ }
   validates :image,             presence: true
