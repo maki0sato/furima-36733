c024c93b8f56c209ef8f35195bd1bbc1c649395e
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-15 10:42:52 +0900

    マイグレーションファイルに記述・itemsテーブル作成

--- db/migrate/20210915012752_create_items.rb
+++ db/migrate/20210915012752_create_items.rb
@@ -1,6 +1,16 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
+      
+      t.string     :item,              null: false
+      t.text       :text,              null: false
+      t.integer    :category_id,       null: false
+      t.integer    :quality_id,        null: false
+      t.integer    :price,             null: false
+      t.integer    :delivery_free_id,  null: false
+      t.integer    :prefecture_id,     null: false
+      t.integer    :days_to_id,        null: false
+      t.references :user,              foreign_key: true
 
       t.timestamps
     end
