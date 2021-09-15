645ca2c45772bdb425d5862e41958152035b94b9
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-15 12:13:56 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,8 @@ class Item < ApplicationRecord
   validates :delivery_free_id,  presence: true
   validates :prefecture_id,     presence: true
   validates :days_to_id,        presence: true
-  validates :price,             presence: true, numericality: {in: 300..9999999}, format: { /\A[0-9]+\z/ }
+  validates :price,             presence: true, numericality: {in: 300..9999999}
+  validates :price,             format: { with: /\A[0-9]+\z/ }
   validates :image,             presence: true
 
 end
