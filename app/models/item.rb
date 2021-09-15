44444d799d121a2c832e50e6ee23659d79a8eddc
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-15 11:22:44 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,15 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+  
+  validates :item,              presence: true
+  validates :text,              presence: true
+  validates :category_id,       presence: true
+  validates :quality_id,        presence: true
+  validates :delivery_free_id,  presence: true
+  validates :prefecture_id,     presence: true
+  validates :days_to_id,        presence: true
+  validates :price,             presence: true, numericality: {in: 300..9999999}, format: { /\A[0-9]+\z/ }
+  validates :image,             presence: true
 
 end
