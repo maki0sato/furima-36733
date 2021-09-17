70e6e0013e18f23a0575e165b8ef586eaa037858
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-17 15:18:30 +0900

    pry-railsの導入とテストコードの記入

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -11,5 +11,25 @@ describe ItemsController, type: :request do
       get root_path
       expect(response.status).to eq 200
     end
+
+    it 'indexアクションのレスポンス内に出品された商品の画像が表示されている' do
+      get root_path
+      expect(response.body).to include(url_for(@item.image))
+    end
+
+    it 'indexアクションのレスポンス内に出品された商品の名前が表示されている' do
+      get root_path
+      expect(response.body).to include(@item.item)
+    end
+    
+    it 'indexアクションのレスポンス内に出品された商品の送料負担が表示されている' do
+      get root_path
+      expect(response.body).to include(@item.delivery_free[:name])
+    end
+
+    it 'indexアクションのレスポンス内に出品された商品の価格が表示されている' do
+      get root_path
+      expect(response.body).to include(@item.price.to_s)
+    end
   end
 end
