a7fcc9fd780bd496c9fa253643f5de22509ebcbb
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-18 13:03:44 +0900

    削除の条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,7 +36,9 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    redirect_to root_path if item.destroy
+    if item.user_id == current_user.id
+      redirect_to root_path if item.destroy
+    end
   end
 
   private
