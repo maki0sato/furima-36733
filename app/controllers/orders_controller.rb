330e1a7daf33e2d7f4f613fd497e66a1fc2e66ce
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-19 15:38:29 +0900

    保存ができるようにコントローラーを編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,19 @@
 class OrdersController < ApplicationController
 
   def create
+    @item = Item.find(params[:id])
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render :order
+    end
   end
-  
+
+  private
+
+  def order_params
+    params.require(:order).merge(params[:item_id] , user_id: current_user.id)
+  end
 end
