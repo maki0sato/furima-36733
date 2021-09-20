90ca53be54a44dfb5e569b073fdf91c4f2eec67e
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-20 15:41:21 +0900

    Formオブジェクトを修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -15,4 +15,5 @@ class OrderAddress
     order = Order.create(item_id: item_id, user_id: user_id)
 
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, telephone_nember: telephone_nember, order_id: order_id)
+  end
 end
