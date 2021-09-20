10e3f50f0300a0a9ee0672d132e011b0157ce491
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-20 19:35:14 +0900

    card.js修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number, :order_id, :item_id, :user_id
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number, :order_id, :item_id, :user_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -12,7 +12,7 @@ class OrderAddress
     validates :user_id
     validates :token
   end
-  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
