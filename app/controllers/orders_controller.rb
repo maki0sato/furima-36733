ceff660b43c5e5ae6ab7c8150fa013ca3153180a
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-20 14:46:38 +0900

    コントローラーを編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,13 +2,16 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-    @order = Order.new
+    @order_address = OrderAddress.new
+  end
+
+  def new
+    @order_address = OrderAddress.new
   end
 
   def create
-    @order = Order.new(order_params)
-    Address.create(address_params)
-    if @order.valid?
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
       pay_item
       @order.save
       return redirect_to root_path
@@ -20,18 +23,18 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).merge(params[:item_id], user_id: current_user.id, token: params[:token])
+    params.require(:order_address).permit(:postal_code, :prefecture, :city, :house_number, :building_name, :telephone_nember).merge(token: params[:token], user_id: current_user.id, order_id: @order.id)
   end
 
-  def address_params
-    params.permit(:postal_code, :prefecture, :city, :house_number, :building_name, :telephone_nember).merge(order_id: @order.id)
-  end
+  #def address_params
+    #params.permit(:postal_code, :prefecture, :city, :house_number, :building_name, :telephone_nember).merge(order_id: @order.id)
+  #end
 
   def pay_item
     Payjp.api_key = "sk_test_ddfdfa9d06fd79d5395d88e1"
     Payjp::Charge.create(
-      amount: order_params[:price],
-      card: order_params[:token],
+      amount: order_address_params[:price],
+      card: order_address_params[:token],
       currency: 'jpy'
     )
   end
