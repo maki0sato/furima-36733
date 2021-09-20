d90db099791216cfb531f630bbb3ffe639b0f611
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-20 15:57:29 +0900

    addressモデルにアソシエーションの追加

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,6 @@
 class Address < ApplicationRecord
   belongs_to :order
-
+  belongs_to :prefecture
   #validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   #validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
   #validates :telephone_nember, numericality: true
