2b21cac9e89f813a2ac24c0cf84d369a8f2fd3d2
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-20 12:42:02 +0900

    アソシエーションを定義

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order < ApplicationRecord
   attr_accessor :token
   belongs_to :user
   belongs_to :item
+  has_one :address
 
   validates :user,  presence: true
   validates :item,  presence: true
