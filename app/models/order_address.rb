31428f9208aa452aaa8a2e2d63f64ba8e9d37a93
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-20 15:48:52 +0900

    バリデーションを修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,13 +1,15 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture, :city, :house_number, :building_name, :telephone_nember, :order_id, :item_id, :user_id, :token
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_nember, :order_id, :item_id, :user_id
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :house_number
+    validates :telephone_nember, format: {with: /\A[0-9]{11}\z/ }
     validates :order_id
     validates :item_id
     validates :user_id
-    validates :token
   end
   validates :prefecture, numericality: {other_than: 1, message: "can't be blank"}
 
