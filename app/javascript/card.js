fbc7f373b287ba5e3191337f91fe31f4eff70d5c
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-20 12:01:30 +0900

    フォーム情報の取得

---
+++ app/javascript/card.js
@@ -0,0 +1,19 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_7a606c8980bff4f12d718972");
+  const submit = document.getElementById("button");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order[number]"),
+      cvc: formData.get("order[cvc]"),
+      exp_month: formData.get("order[exp_month]"),
+      exp_year: `20${formData.get("order[exp_year]")}`,
+    };
+  });
+};
+
+window.addEventListener("load", pay);
