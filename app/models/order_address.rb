f1ee4a4d192a51cbb1441cda6ec21b2d749c84a7
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-21 17:09:29 +0900

    formオブジェクトを修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -14,7 +14,7 @@ class OrderAddress
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
-    order = Order.create(item_id: item.id, user_id: user.id)
+    order = Order.create(item_id: item_id, user_id: user_id)
 
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, telephone_number: telephone_number, order_id: order.id)
   end
--- db/migrate/20210920032829_create_addresses.rb
+++ db/migrate/20210920032829_create_addresses.rb
@@ -7,7 +7,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.string     :city,             null: false
       t.string     :house_number,     null: false
       t.string     :building_name
-      t.string     :telephone_nember, null: false
+      t.integer    :telephone_nember, null: false
       t.references :order,            null: false, foreign_key: true
 
       t.timestamps
