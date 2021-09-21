55c29ac4184c06585ee415829abf779472bceb2d
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-21 18:02:04 +0900

    ActiveHashを使用したアソシエーションを定義

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,5 @@
 class Address < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions 
   belongs_to :order
   belongs_to :prefecture
   #validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
