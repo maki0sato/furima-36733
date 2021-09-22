e247b87d78ba80d905b924e313964ffeb20b096e
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-22 16:45:30 +0900

    不要なコメントアウト、記述を削除

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,6 @@
 class Order < ApplicationRecord
-  attr_accessor :token
-
   belongs_to :user
   belongs_to :item
   has_one :address
 
-  # validates :user,  presence: true
-  # validates :item,  presence: true
-  # validates :token, presence: true
 end
