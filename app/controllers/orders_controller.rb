7628aa402a86d5b22f16f1a1b2c478b2e6de85aa
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-22 12:22:28 +0900

    不要なコメントアウトの削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,7 +18,6 @@ class OrdersController < ApplicationController
   end
 
   def create
-    # @order = Order.new
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
@@ -36,9 +35,6 @@ class OrdersController < ApplicationController
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number).merge(user_id: current_user.id, token: params[:token], item_id: params[:item_id])
   end
 
-  #def address_params
-    #params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_nember).merge(order_id: @order.id)
-  #end
 
   def pay_item
     Payjp.api_key = "sk_test_ddfdfa9d06fd79d5395d88e1"
