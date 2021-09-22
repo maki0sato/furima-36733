4dde93ab33cedaaf4f88c187d8403f45c11d3727
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-22 11:58:09 +0900

    テストコードを記述

--- spec/factories/order_address.rb
+++ spec/factories/order_address.rb
@@ -1,14 +1,11 @@
 FactoryBot.define do
   factory :order_address do
-    transient do
-      person { Gimei.name }
-    end
     token                 { "tok_abcdefghijk00000000000000000" }
-    postal_code           { Faker::Postcode.to_s.insert(3, "-")}
+    postal_code           { "000-0000"}
     prefecture_id         { Faker::Number.between(from: 2, to: 48) }
-    city                  { address.city.kanji }
-    house_number          { address.town.kanji }
-    building_name         { address.town.kanji }
+    city                  { "渋谷区" }
+    house_number          { "宇田川町" }
+    building_name         { "ビル" }
     telephone_number      { "09000000000" }
   end
 end
